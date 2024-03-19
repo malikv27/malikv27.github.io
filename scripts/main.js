5ef2a56394009eb2ca06eac814a1534970b3d9cf
malikv27 <148505062+malikv27@users.noreply.github.com>
2024-03-19 01:42:53 -0400

    Main final changes

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+function aboutAlert(){
+    alert("You've reached the About Page!");
+}
+
+function portfolioAlert(){
+    alert("You've reached the Portfolio Page!");
+}
+
+function resumeAlert(){
+    alert("You've reached the Resume Page!");
+}
